8f83ad7603adcc9481e7305c525c0d7d458ebfbf
Gabriel <devbiel@protonmail.com>
2023-09-29 20:39:01 -0300

    atualizando e melhorando a calculadora

--- Exercicios/Exercicio_Calculadora/assets/js/app.js
+++ Exercicios/Exercicio_Calculadora/assets/js/app.js
@@ -41,104 +41,58 @@ function Calculadora() {
 
     this.botoesTeclado = function () {
         document.addEventListener('keyup', e => {
-            if (e.key === '1') {
-                this.display.value += '1';
-            }
+            if (e.key === '1') this.display.value += '1';
 
-            if (e.key === '2') {
-                this.display.value += '2';
-            }
+            if (e.key === '2') this.display.value += '2';
 
-            if (e.key === '3') {
-                this.display.value += '3';
-            }
+            if (e.key === '3') this.display.value += '3';
 
-            if (e.key === '4') {
-                this.display.value += '4';
-            }
+            if (e.key === '4') this.display.value += '4';
 
-            if (e.key === '5') {
-                this.display.value += '5';
-            }
+            if (e.key === '5') this.display.value += '5';
 
-            if (e.key === '6') {
-                this.display.value += '6';
-            }
+            if (e.key === '6') this.display.value += '6';
 
-            if (e.key === '7') {
-                this.display.value += '7';
-            }
+            if (e.key === '7') this.display.value += '7';
 
-            if (e.key === '8') {
-                this.display.value += '8';
-            }
+            if (e.key === '8') this.display.value += '8';
 
-            if (e.key === '9') {
-                this.display.value += '9';
-            }
+            if (e.key === '9') this.display.value += '9';
 
-            if (e.key === '0') {
-                this.display.value += '0';
-            }
+            if (e.key === '0') this.display.value += '0';
 
-            if (e.key === '+') {
-                this.display.value += '+';
-            }
+            if (e.key === '+') this.display.value += '+';
 
-            if (e.key === '-') {
-                this.display.value += '-';
-            }
+            if (e.key === '-') this.display.value += '-';
 
-            if (e.key === '*') {
-                this.display.value += '*';
-            }
+            if (e.key === '*') this.display.value += '*';
 
-            if (e.key === '/') {
-                this.display.value += '/';
-            }
+            if (e.key === '/') this.display.value += '/';
 
-            if (e.key === ',') {
-                this.display.value += ',';
-            }
+            if (e.key === ',') this.display.value += ',';
 
-            if (e.key === 'Backspace') {
-                this.botaoApagar();
-            }
+            if (e.key === 'Backspace') this.botaoApagar();
 
-            if (e.key === 'Enter') {
-                this.botaoIgual();
-            }
+            if (e.key === 'Enter') this.botaoIgual();
         })
 
     };
 
     this.enviarEnter = function () {
-        document.addEventListener('keyup', e => {
-            if (e.key === 13) {
-                this.botaoIgual();
-            }
-        })
+        document.addEventListener('keyup', e => { if (e.key === 13) this.botaoIgual(); })
     };
 
     this.clickBotoes = function () {
         document.addEventListener('click', (e) => {
             const el = e.target;
 
-            if (el.classList.contains('btn-num')) {
-                this.btnParaDisplay(el.innerText);
-            }
+            if (el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText);
 
-            if (el.classList.contains('btn-clear')) {
-                this.botaoClear();
-            }
+            if (el.classList.contains('btn-clear')) this.botaoClear();
 
-            if (el.classList.contains('btn-del')) {
-                this.botaoApagar();
-            }
+            if (el.classList.contains('btn-del')) this.botaoApagar();
 
-            if (el.classList.contains('btn-eq')) {
-                this.botaoIgual();
-            }
+            if (el.classList.contains('btn-eq')) this.botaoIgual();
 
         });
     }
@@ -226,4 +180,4 @@ function criaCalculadora() {
 const calculadora = criaCalculadora();
 calculadora.inicia();
  
-*///
+*/
